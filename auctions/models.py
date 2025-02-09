987a9dbec1bd394fed19fcaf9c45f5f50684845c
Maz <mazoolander@me.com>
2025-02-09 16:46:23 +0100

    Add initial migration files, reset CSS, and update layout template

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# add additional models to this file to represent details about auction listings, bids, comments, and auction categories. 
+# Remember that each time you change anything in auctions/models.py, you’ll need to first run python manage.py makemigrations and then python manage.py migrate to migrate those changes to your database.
 
 class User(AbstractUser):
     pass
